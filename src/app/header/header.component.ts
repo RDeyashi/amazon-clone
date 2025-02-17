b8a25818d06edd785d5873a4b45295a1b0d5e11d
Rabi Deyashi <engg.r.deyashi@gmail.com>
2025-02-17 23:45:01 +0530

    Header fix

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   public orderType:string[] = ["All Categories", "Amazon Fresh", "Alexa Skills", "Amazon Devices", "Amazon Freshers"]
+  public subHeaderOption: string[] = ["Fresh", "Mobiles", "Gift Cards", "Sell", "Gift Ideas", "MX Player", "Toys & Games", "Audible", "Grocery & Gourmet Foods", "Today's Deal", 
+    "Sports, Fitness & Outdoors", "AmazonBasics", "Pet Supplies"]
 }
