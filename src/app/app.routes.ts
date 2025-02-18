d83dd284979f44a7bcb99bfa1819d45e3371273d
Rabi Deyashi <rdeyashi17@gmail.com>
2025-02-18 20:28:05 +0530

    main and not found component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: MainComponent},
     {
         path: 'fresh',
         loadComponent: () => import("fresh/HomeComponent").then(m => m.HomeComponent)
